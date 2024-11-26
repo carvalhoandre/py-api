d07daeed915ff55096257237c5217907e26750f5
carvalhoandre <andre_carvalho0@live.com>
2024-11-26 15:53:09 -0300

    fix:  tetrieves port number from the environment variable

--- main.py
+++ main.py
@@ -64,4 +64,7 @@ def excluir_livro(id):
     return jsonify(livros)
 
 
-app.run(port=5000, host='localhost', debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
